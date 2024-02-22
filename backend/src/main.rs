0f7ac19d5fcd55085b8d5bb54af4d3c60271dd1c
Max Walters <mdwalters.pm@proton.me>
2024-02-22 01:14:47 +0000

    chore: bro what

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,18 @@
+#[allow(unused_imports)] // yeah we have issues with skill
 #[macro_use] extern crate rocket;
 use rocket::serde::json::{Value, json};
 use mongodb::{
     bson::{doc, Document},
-    error::Error,
+    error::Error as MongoError,
     Client,
     Collection,
     Database,
 };
+use std::env;
+use std::error::Error as ActualError;
 use shared::{Colours};
+
+struct MongoData { database: Database }
 
 #[get("/")]
 fn index() -> Value {
@@ -59,7 +64,7 @@ fn wall(username: &str) -> Value {
                     "id": "",
                     "color": "",
                 },
-                "parent": None,
+                "parent": "", // supposed to be null but because of rust's stance on being the most memory safe language of the yet,,,
                 "content": "",
                 "time": 0,
                 "hasReplies": false,
@@ -69,11 +74,31 @@ fn wall(username: &str) -> Value {
     })
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![
-        index,
-        users,
-        wall
-    ])
+#[rocket::main]
+async fn main() -> Result<(), Box<dyn ActualError>> {
+    let mut url: String = "placeholder".to_string();
+    dotenvy::dotenv()?;
+
+    match env::var("WOT_MONGODB") {
+        Ok(val) => url = format!("{val}"),
+        Err(e) => println!("{e}"),
+    }
+
+    let client = Client::with_uri_str(url)
+        .await
+        .expect("Failed to connect to MongoDB, possibly skill issue on your end.");
+    let mongo: Database = client.database("wasteof-time");
+
+    rocket::build()
+        .manage(mongo)
+        .mount("/", routes![
+            index,
+            users,
+            wall
+        ])
+        .launch()
+        .await
+        .expect("Rocket failed to launch, probably skill issue on Max's end.");
+
+    Ok(())
 }
